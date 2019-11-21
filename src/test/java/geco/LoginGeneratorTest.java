0f2710e6c04bbaa24474c94e542fbcf4f1665366
Aymeric PINEL <aymeric.pinel@gmail.com>
2019-11-21 10:09:30 +0100

    Ajout tests LoginGenerator #2 et résolution bug suffixe fix #3

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,42 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    LoginService ls;
+    LoginGenerator lg;
+
+    @Before
+    public void setUp(){
+        ls = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDU", "JRAL", "JRAL1"});
+        lg = new LoginGenerator(ls);
+    }
+
+    @Test
+    public void testGenerateLoginForNomAndPrenom() {
+        lg.generateLoginForNomAndPrenom("DURAND", "Paul");
+        assertTrue("Paul DURAND login should be PDUR", ls.loginExists("PDUR"));
+    }
+
+    @Test
+    public void testAjoutChangeant(){
+        lg.generateLoginForNomAndPrenom("ROLLING", "Jean");
+        assertTrue("Jean ROLLING login should be JROL1", ls.loginExists("JROL1"));
+    }
+
+    @Test
+    public void testAjoutAccent(){
+        lg.generateLoginForNomAndPrenom("Dùrand", "Paul");
+        assertTrue("Paul Dùrand login should be PDUR", ls.loginExists("PDUR"));
+    }
+
+    @Test
+    public void testTroisiemeAjout(){
+        lg.generateLoginForNomAndPrenom("RALLING", "John");
+        assertTrue("New John Ralling should be JRAL2", ls.loginExists("JRAL2"));
+    }
+}
